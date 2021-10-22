98ed134242d8e5b69b2045d8962cdf572c85184a
Jason Quiroga <jason.quiroga@mymail.champlain.edu>
2021-10-22 11:30:53 -0700

    Week 8 Security Events and Email Assignments.

---
+++ Securityeventlog.ps1
@@ -0,0 +1,19 @@
+﻿# Storyline: Review the Security Event Log
+
+# List all the available Windows Event Logs
+Get-EventLog -list
+
+# Directory to Save Files
+$myDir = "C:\Users\jason-adm\Desktop"
+
+# Create a prompt to allow the user to select the Log to view
+$readLog = Read-host -Prompt "Please select a log to review from the list above"
+
+
+# Task: Create a prompt that allows the user to specify a keyword or phrase to search on.
+# Find a string from your event logs to search on
+
+$readPhrase = Read-host -Prompt "Please Specify a Keyword or Phrase to search the logs for"
+
+# Print the results for the log
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$readPhrase*" } | export-csv -NoTypeInformation ` -Path "$myDir\securityLogs.csv"
---
+++ week8email.ps1
@@ -0,0 +1,16 @@
+﻿# Storyline: Send an email.
+
+# Body of the email
+$msg = "Hello there."
+
+# Echoing to the screen.
+write-host -BackgroundColor Red -ForegroundColor White "$msg"
+
+# Email from Address
+$email = "jason.quiroga@mymail.champlain.edu"
+
+# Email to Address
+$toEmail = "deployer@csi-web"
+
+# Send the email
+Send-MailMessage -From $email -to $toEmail -Subject "A Greeting" -Body $msg -SmtpServer 192.168.6.71
